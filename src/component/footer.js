7c43a13b82112fa5aec77c03786efd361ce1ceb2
aabdulmaulana <57434451+aabdulmaulana@users.noreply.github.com>
2023-03-24 10:34:57 +0700

    Update footer.js

---
+++ src/component/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <h2>Copyright @abdulmaulana</h2>
+      <p>Website ini dibuat menggunakan ReactJS</p>
+    </footer>
+  );
+}
+
+export default Footer;
